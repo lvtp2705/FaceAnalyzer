b453501df92cf2366b60bf02fca3ff6bd9a1bfe9
Le Vo Tien Phat <69445507+lvtp2705@users.noreply.github.com>
2021-10-12 15:12:13 +0700

    Update predict_age.py

--- Age Detection using OpenCV/predict_age.py
+++ Age Detection using OpenCV/predict_age.py
@@ -6,10 +6,8 @@ import numpy as np
 import argparse
 
 # The model architecture
-# download from: https://drive.google.com/open?id=1kiusFljZc9QfcIYdU2s7xrtWHTraHwmW
 AGE_MODEL = 'weights_age_detect/deploy_age.prototxt'
 # The model pre-trained weights
-# download from: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
 AGE_PROTO = 'weights_age_detect/age_net.caffemodel'
 # Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
 # substraction to eliminate the effect of illunination changes
@@ -17,9 +15,7 @@ MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 # Represent the 8 age classes of this CNN probability layer
 AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)',
                  '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
-# download from: https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
 FACE_PROTO = "weights_age_detect/deploy.prototxt.txt"
-# download from: https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
 FACE_MODEL = "weights_age_detect/res10_300x300_ssd_iter_140000_fp16.caffemodel"
 
 # Initialize frame size
